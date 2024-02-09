8f3bd5c425af9196f14521d8f83db85e8f13d409
KRmiyamoto <miyamoto@grinnell.edu>
2024-02-09 14:25:42 -0600

    2-9 lab

--- src/MathExpt.java
+++ src/MathExpt.java
@@ -16,6 +16,38 @@ public class MathExpt {
       pen.println(root + "^2 = " + (root * root));
     } // for i
 
+    // Exercise 1: Square roots
+    // (c) Print more sqrts
+    // Integer
+    double rootInt = MathUtils.squareRoot(4);
+    pen.println("The square root of " + 4 + " seems to be " + rootInt);
+    pen.println(rootInt + "^2 = " + (rootInt * rootInt));
+
+    // Float
+    double rootFloat = MathUtils.squareRoot((float) 4.0);
+    pen.println("The square root of " + 4.0 + " seems to be " + rootFloat);
+    pen.println(rootFloat + "^2 = " + (rootFloat * rootFloat));
+
+    // Double
+    double rootDouble = MathUtils.squareRoot((Double) 2.25);
+    pen.println("The square root of " + 2.25 + " seems to be " + rootDouble);
+    pen.println(rootDouble + "^2 = " + (rootDouble * rootDouble));
+
+    // BigInteger
+    double rootBigInt = MathUtils.squareRoot(java.math.BigInteger.valueOf(16));
+    pen.println("The square root of " + 16 + " seems to be " + rootBigInt);
+    pen.println(rootBigInt + "^2 = " + (rootBigInt * rootBigInt));
+
+    // BigDecimal
+    double rootBigD = MathUtils.squareRoot(java.math.BigDecimal.valueOf(2.25));
+    pen.println("The square root of " + 2.25 + " seems to be " + rootBigD);
+    pen.println(rootBigD + "^2 = " + (rootBigD * rootBigD));
+
+    // double
+    double rootLittleDoub = MathUtils.squareRoot((double) 2.25);
+    pen.println("The square root of " + 2.25 + " seems to be " + rootLittleDoub);
+    pen.println(rootLittleDoub + "^2 = " + (rootLittleDoub * rootLittleDoub));
+
     // We're done. Clean up.
     pen.close();
   } // main(String[])
--- src/TBExpt.java
+++ src/TBExpt.java
@@ -15,11 +15,53 @@ public class TBExpt {
     // Prepare for input and output
     PrintWriter pen = new PrintWriter(System.out, true);
 
+    
+    // Exercise 2: Simple Lines
     // Create a block to use
-    TextBlock block = new TextLine("Hello");
-
+    TextBlock block = new TextLine("This is a test.");
     // Print out the block
     TBUtils.print(pen, block);
+
+    // Exercise 3: Boxed Text
+    // (a) Boxed line
+    TextBlock boxed = new TextLine("Hello|");
+    pen.println("+" + TBUtils.dashes(5) + "+");
+    pen.print("|");
+    TBUtils.print(pen, boxed);
+    pen.println("+" + TBUtils.dashes(5) + "+");
+
+    // (b) Boxed boxed line
+    TextBlock boxedBoxed = new TextLine("Hello||");
+    pen.println("+" + TBUtils.dashes(7) + "+");
+    pen.println("|+" + TBUtils.dashes(5) + "+|");
+    pen.print("||");
+    TBUtils.print(pen, boxedBoxed);
+    pen.println("|+" + TBUtils.dashes(5) + "+|");
+    pen.println("+" + TBUtils.dashes(7) + "+");
+    // (c) Boxed empty line
+    TextBlock empty = new TextLine("|");
+    pen.println("+" + TBUtils.dashes(1) + "+");
+    pen.print("| ");
+    TBUtils.print(pen, empty);
+    pen.println("+" + TBUtils.dashes(1) + "+");
+
+
+    // Exercise 4: Composing Text
+    TextBlock hello = new TextLine("Hello");
+    TextBlock goodbye = new TextLine("Goodbye");
+    // (a)
+    BoxedBlock vertComp = new BoxedBlock (new VComposition(hello, goodbye));
+    TBUtils.print(pen, vertComp);
+    // (b)
+    VComposition vertComp2 = new VComposition(new BoxedBlock(hello), new BoxedBlock(goodbye));
+    TBUtils.print(pen, vertComp2);
+    // (c)
+    HComposition horComp = new HComposition(new BoxedBlock(hello), goodbye);
+    TBUtils.print(pen, horComp);
+    // (d)
+    HComposition horComp2 = new HComposition(goodbye, new BoxedBlock(hello));
+    TBUtils.print(pen, horComp2);
+
 
     // Clean up after ourselves.
     pen.close();
